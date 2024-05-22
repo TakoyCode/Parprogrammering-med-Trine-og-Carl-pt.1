c4b0a24b08901c9a840e087f4782bf1be4545df6
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-05-22 14:45:42 +0200

    Add project files.

---
+++ Parprogrammering med Trinne og Carl/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parprogrammering_med_Trinne_og_Carl
+{
+
+    //Lager en 'oppskrift' hvor du lager en mal for det du skal lage
+    internal class Person
+    {
+       public string Name;
+       public int Age;
+       public string HairColor;
+       public string Hobby;
+       public string WhatDoing;
+       public string Textbox;
+
+       //Tom constructor som gjør at du kan få inn alle metodene
+       public Person()
+       {
+       }
+
+       //Constructor som gjør at du kan sette verdiene direkte via constructor og inn i objekter.
+       public Person(string name, int age, string haircolor, string hobby, string whatdoing)
+       {
+           Name = name;
+           Age = age;
+           HairColor = haircolor;
+           Hobby = hobby;
+           WhatDoing = whatdoing;
+           Textbox = $"Hei , jeg heter {Name} Jeg er {Age} år gammel og jeg har {HairColor} hår,";
+       }
+
+       public void ShowPeople()
+       {
+           Console.Clear();
+           Console.WriteLine(Textbox);
+           Console.WriteLine(Hobby);
+           Console.WriteLine(WhatDoing);
+       }
+    }
+}
---
+++ Parprogrammering med Trinne og Carl/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Xml.Linq;
+
+namespace Parprogrammering_med_Trinne_og_Carl
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to chaos!");
+                Console.WriteLine("");
+                Console.WriteLine("Choose what you wish to do");
+                Console.WriteLine("1 Meet the gang!");
+                Console.WriteLine("2 Exit App");
+
+                string response = Console.ReadLine();
+                if (response == "1")
+                {
+                    MeetTheGang();
+                } else if (response == "2")
+                {
+                    Environment.Exit(69);
+                }
+
+            } 
+            void MeetTheGang()
+            {
+                while (true)
+                {
+                    Console.WriteLine("\n1 Carl");
+                    Console.WriteLine("2 Trine");
+                    Console.WriteLine("3 Audun");
+                    Console.WriteLine("4 Back to main");
+                    string Reaction = Console.ReadLine();
+                    if (Reaction == "1")
+                    {
+                        Carl();
+                    }
+                    else if (Reaction == "2")
+                    {
+                        Trine();
+                    }
+                    else if (Reaction == "3")
+                    {
+                        Audun();
+                    }
+                    else if (Reaction == "4")
+                    {
+                        Main();
+                    }
+                }
+            }
+
+            void Carl()
+            {
+                //Du kaller og setter de forskjellige verdiene
+                Person Carl = new Person();
+                Carl.Name = "Carl";
+                Carl.Age = 23;
+                Carl.HairColor = "Blond";
+                Carl.Hobby = "Nå koder jeg!";
+                Carl.WhatDoing = "Se på meg! Jeg har blitt et dataprogram!";
+                Carl.Textbox = $"Hei , jeg heter {Carl.Name} Jeg er {Carl.Age} år gammel og jeg har {Carl.HairColor} hår,";
+
+                Carl.ShowPeople();
+            }
+
+            void Trine()
+            {
+                //Her lages en person og verdiene settes direkte
+                Person Trine = new Person
+                {
+                    Name = "Trine",
+                    Age = 31,
+                    HairColor = "Brunt",
+                    Hobby = "Gaming og lese bøker!",
+                    WhatDoing = "Jeg prøver å lære meg C# og gråter innvendig!",
+                };
+                Trine.Textbox = $"Hei , jeg heter {Trine.Name} Jeg er {Trine.Age} år gammel og jeg har {Trine.HairColor} hår,";
+                
+                Trine.ShowPeople();
+            }
+
+            void Audun()
+            {
+                //Du sender med de forskjellige verdiene du vil ha inn i constructoren
+                Person Audun = new Person("Audun", 22, "Brunt", "Jeg stirrer i takket.", "Det er gøy å svinge rundt på stolen.");
+                Audun.ShowPeople();
+            }
+        }
+
+     
+    }
+}
+
+
+
+
